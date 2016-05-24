d38f57ec78415be2907ab7824903a720117373e5
Lorenzo1975 <lorenzo.beltrame@gmail.com>
2016-05-24 12:07:37 +0200

    update to lecture 200 or so, added select on form fiels

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
+use App\Role;
 
 class AdminUsersController extends Controller
 {
@@ -17,7 +19,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -29,7 +33,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');        
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));        
     }
 
     /**
@@ -41,6 +47,8 @@ class AdminUsersController extends Controller
     public function store(Request $request)
     {
         //
+
+        return $request->all();
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,40 @@
-<h1>Is working</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Users</h1>
+
+<table class="table">
+	<thead>
+	<th>Id</th>
+	<th>Name</th>
+	<th>Email</th>
+	<th>Role</th>
+	<th>Status</th>
+	<th>Created</th>
+	<th>Updated</th>
+	</thead>
+	<tbody>
+
+	@if($users)
+
+	@foreach($users as $user)
+	<tr>
+		<td>{{$user->id}}</td>
+		<td>{{$user->name}}</td>
+		<td>{{$user->email}}</td>				
+		<td>{{$user->role->name}}</td>
+
+		<td>{{$user->is_active == 1 ? 'active' : 'not active' }}</td>
+
+		<td>{{$user->created_at->diffForHumans()}}</td>				
+		<td>{{$user->updated_at->diffForHumans()}}</td>				
+	</tr>		
+	
+	@endforeach
+
+	@endif
+	</tbody>
+</table>
+
+@endsection
